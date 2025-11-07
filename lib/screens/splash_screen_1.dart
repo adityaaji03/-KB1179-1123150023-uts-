5bd45985414cb4a58043d7e9e858c10bd516727e
adityaaji03 <adityaajipramono03@gmail.com>
2025-11-07 20:38:18 +0700

    create page splash screen scafold awal

---
+++ lib/screens/splash_screen_1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        // TODO: isi widget di sini
+      ),
+    );
+  }
+}
