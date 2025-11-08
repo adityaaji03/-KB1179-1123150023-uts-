849df4de8f5054b691eaad5d321e7efd392e92c1
adityaaji03 <adityaajipramono03@gmail.com>
2025-11-08 19:57:54 +0700

    membuat scafold login

---
+++ lib/screens/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Login());
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text("Halaman Login"))),
+    );
+  }
+}
